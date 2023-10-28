d999b6d816b429938a616c68c755ab7adadd9ad5
romeo <rjameson1928@gmail.com>
2023-10-28 17:17:40 +0300

    HW-8 update homeWorkReducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,11 +7,11 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
 	switch (action.type) {
 		case 'sort':  // by name
-				if (action.payload === "up") {
-					return [...state].sort((a, b) => a.name.localeCompare(b.name));
-				} else if (action.payload === "down") {
-					return [...state].sort((a, b) => b.name.localeCompare(a.name));
-				}
+			if (action.payload === "up") {
+				return [...state].sort((a, b) => a.name.localeCompare(b.name));
+			} else if (action.payload === "down") {
+				return [...state].sort((a, b) => b.name.localeCompare(a.name));
+			}
 			// ? : тернарник
 			// return action.payload === 'up'
 			// 	? [...state].sort((a, b) => a.name.localeCompare(b.name))
@@ -21,6 +21,8 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
 			break;
 		case 'check': {
 			return state.filter((el) => el.age >= 18);
+			// фильтрация + сортировка по возрастанию
+			// return state.filter((el) => el.age >= 18).sort((a, b) => a.age - b.age);
 		}
 		default:
 			return state
