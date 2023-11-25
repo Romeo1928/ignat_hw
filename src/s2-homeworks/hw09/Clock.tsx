dc6b30fe20f6b86945224ba47523014d63248e28
romeo <rjameson1928@gmail.com>
2023-11-25 20:36:57 +0300

    HW-9 update

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -42,12 +42,24 @@ function Clock() {
 		setShow(false)
 	}
 
-	const stringTime = `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}` ||
-		<br/>  // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-	const stringDate = `${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()}` ||
-		<br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+	// создал функцию, чтобы убрать дублирование .toString().padStart(2, '0')
+	function formatWithZero(num: number) {
+		return num.toString().padStart(2, '0');
+	}
 
-// день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
+	// 1-й способ без дублирования .toString().padStart(2, '0')
+	const stringTime = `${formatWithZero(date.getHours())}:${formatWithZero(date.getMinutes())}:${formatWithZero(date.getSeconds())}` ||
+		<br/>
+	const stringDate = `${formatWithZero(date.getDate())}:${formatWithZero(date.getMonth())}:${date.getFullYear()}` ||
+		<br/>
+
+	// 2-й способ
+	// const stringTime = `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}` ||
+	// 	<br/>  // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+	// const stringDate = `${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()}` ||
+	// 	<br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+
+	// день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
 	const stringDay = `${new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date)}` || <br/> // пишут студенты
 	const stringMonth = `${new Intl.DateTimeFormat('en-US', {month: 'long'}).format(date)}` || <br/> // пишут студенты
 
