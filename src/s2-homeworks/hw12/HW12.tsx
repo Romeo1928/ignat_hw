7c96d84167e0d0e32d2805e44a7101ba3d9b414b
romeo <rjameson1928@gmail.com>
2023-12-25 23:26:39 +0300

    HW-12

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -4,6 +4,7 @@ import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import {useDispatch, useSelector} from 'react-redux'
 import {changeThemeId} from './bll/themeReducer'
+import {AppStoreType} from "../hw10/bll/store";
 
 /*
 * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
@@ -12,19 +13,25 @@ import {changeThemeId} from './bll/themeReducer'
 * 4 - передать пропсы в SuperSelect
 * */
 
-const themes = [
-    {id: 1, value: 'light'},
-    {id: 2, value: 'blue'},
-    {id: 3, value: 'dark'},
+const themes: ThemesType[] = [
+   {id: 1, value: 'light'},
+   {id: 2, value: 'blue'},
+   {id: 3, value: 'dark'},
 ]
 
+export type ThemesType = {
+   id: number
+   value: string
+}
+
 const HW12 = () => {
-    // взять ид темы из редакса
-    const themeId = 1
+   // взять ид темы из редакса
+   const themeId = useSelector<AppStoreType>((state) => state.theme.themeId)
+   const dispatch = useDispatch()
 
-    const change = (id: any) => { // дописать функцию
-
-    }
+   const change = (id: number) => { // дописать функцию
+      dispatch(changeThemeId(id))
+   }
 
     useEffect(() => {
         document.documentElement.dataset.theme = themeId + ''
@@ -41,7 +48,8 @@ const HW12 = () => {
                     id={'hw12-select-theme'}
                     className={s.select}
                     // сделать переключение тем
-
+                    options={themes}
+                    onChangeOption={change}
                 />
             </div>
         </div>
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,24 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
 
+export const themeReducer = (state = initState, action: ChangeThemeIdActionType): InitialStateType => { // fix any
+    switch (action.type) {
+       // дописать
+        case "SET_THEME_ID":
+            return {...state, themeId: action.id}
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+// action
+export const changeThemeId = (id: number): ChangeThemeIdActionType => ({type: 'SET_THEME_ID', id}) // fix any
+
+// types
+type InitialStateType = typeof initState
+
+type ChangeThemeIdActionType = {
+    type: 'SET_THEME_ID',
+    id: number
+}
